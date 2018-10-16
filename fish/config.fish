dfc48d462bae656a25a94273ea698515aedaf88a
HazzaCheng <hazzacheng@gmail.com>
2018-10-16 11:53:09 +0800

    change structure

--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-set -xg GOROOT /opt/go
-set -xg GOPATH ~/work/go
-set -xg PATH /usr/local/sbin /usr/local/bin /usr/bin
-set -xg PATH $GOPATH/bin $PATH
-set -xg PATH ~/work/perl/perf-tools/bin $PATH
-set -xg PATH ~/work/perl/FlameGraph $PATH
-set -xg PATH ~/work/perl/HeatMap $PATH
-set -xg BROWSER google-chrome-stable
